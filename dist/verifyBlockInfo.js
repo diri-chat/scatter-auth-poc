cc311141f814d290de5c6695f707984d064ee04b
John Williamson <johnjwilliamson@live.com>
2019-02-27 13:49:27 +1000

    dist update

--- dist/verifyBlockInfo.js
+++ dist/verifyBlockInfo.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
+const moment = require("moment");
 exports.default = (blockNumber, blockId, expireMinutes) => __awaiter(this, void 0, void 0, function* () {
     const blockData = yield app_1.rpc.get_block(blockNumber);
     const realBlockId = blockData.id.slice(-12).toUpperCase();
@@ -16,10 +17,15 @@ exports.default = (blockNumber, blockId, expireMinutes) => __awaiter(this, void 
     const blocksMatch = claimedBlockId === realBlockId;
     if (!blocksMatch)
         throw "Blocks claimed in signature dont match";
-    const realBlocktime = new Date(blockData.timestamp);
-    const timeNow = new Date();
-    const minutesDifference = (timeNow - realBlocktime) / 60000;
-    if (minutesDifference > expireMinutes)
-        throw `Sign time is greater than expired time of ${expireMinutes} minutes.`;
+    const irreversibleBlockGraceMinutes = 2.5;
+    const signatureTime = moment.utc(blockData.timestamp);
+    const timeCutOff = moment
+        .utc()
+        .subtract(expireMinutes, "minutes")
+        .subtract(irreversibleBlockGraceMinutes, "minutes");
+    const secondsSpare = signatureTime.diff(timeCutOff, "seconds");
+    console.log(`Signature timestamp difference is ${secondsSpare} seconds.`);
+    if (moment(signatureTime).isBefore(timeCutOff))
+        throw `Signature is signed with Block ID too old from now.`;
     return true;
 });
